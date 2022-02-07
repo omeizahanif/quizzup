619303bf55be0d793d8a9496ef60ab35f14efb59
Hanif <omeizahanif@gmail.com>
2022-02-07 11:08:27 +0100

    integrated trivia API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Intro from './components/Intro';
@@ -6,25 +6,53 @@ import Question from './components/Question';
 import Answer from './components/Answer';
 
 function App() {
-    const [overlay, showOverlay] = useState(true)
-    const arr = ["Adios", "Hola", "Au Revoir", "Salir"]
-    const answerList = arr.map(value => <Answer ans={value}/>)
-    
-    
-    function startQuiz() {
-        showOverlay(prevOverlay => !prevOverlay);
-    }
-
-    return (
-        <main>
-            {overlay && <Intro startQuiz={startQuiz}/>}
-            <Question 
-                ques={"How would one say goodbye in Spanish?"}
-                answerList={answerList}
-            /> 
-        </main>
-               
-    )
+  const [overlay, showOverlay] = useState(true)
+  const [questions, setQuestions] = useState([])
+  const [allData, setAllData] = useState([])
+  
+  useEffect(() => {
+      async function fetchQuestions(url) {
+          try {
+              const res = await fetch(url)
+              const data = await res.json()
+              return setAllData(data.results)
+              
+          } catch(err) {
+              console.log("oops! something happened! ", err)
+          }
+      }
+      
+      fetchQuestions(`https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple`)
+  }, [])
+  
+  function startQuiz() {
+      showOverlay(prevOverlay => !prevOverlay);
+      const questionList = allData.map(item => {
+              const answerArray = [item.correct_answer, ...item.incorrect_answers]
+              //const shuffleAnswers = shuffleArray(answerArray)
+              return <Question 
+                      ques={item.question}
+                      answerList={
+                          answerArray.map(answer => <Answer ans={answer}/>)
+                      }
+                      correctAnswer={item["correct_answer"]}
+                  />
+              })
+      setQuestions(questionList);
+  }
+  
+  function shuffleArray(arr) {
+      return arr.sort(() => Math.random() - 0.5);
+  }
+  
+  
+  return (
+      <main>
+          {overlay && <Intro startQuiz={startQuiz}/>}
+          <div>{questions ? questions : "Loading"}</div>
+      </main>
+             
+  )
 }
 
 export default App;

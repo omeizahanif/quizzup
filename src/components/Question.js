67742ef23a43b221fbd7caf0cb1a6f44179e5ba8
Hanif <omeizahanif@gmail.com>
2022-02-07 11:07:53 +0100

    added correct answer span

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,9 +3,10 @@ import {useState, useEffect} from  "react"
 export default function Question(props) {
     
     return (
-        <div>
+        <div className="question-block">
             <p>{props.ques}</p>
             <div>{props.answerList}</div>
+            <span className="correctAnswer">{props.correctAnswer}</span>
         </div>
     )
 }

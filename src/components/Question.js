3164eaf74191cd02a5e2636bd552db988cfcf421
Hanif <omeizahanif@gmail.com>
2022-02-16 08:30:36 +0100

    undo changes

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,43 +1,9 @@
-import { useState } from 'react';
-import Answer from './Answer';
-
-
-
 export default function Question(props) {
-    const [answers, setAnswers] = useState(props.answerList);
-
-    const answerEls = answers.map(eachAnswer => {
-                            return <Answer ans={eachAnswer.value} 
-                            key={eachAnswer.id}
-                            isSelected={eachAnswer.isSelected}
-                            setColor={() => setColor(eachAnswer.id)}/>
-                        })
-    
-    function setColor(id) {
-        /*setQuestions(prevQues => {
-            return prevQues.map(eachQuestion => {
-                const updatedAnswers = changeSelection(eachQuestion.answers, id);
-                return { ...eachQuestion, answers: updatedAnswers}
-               --> <div className="answer-list">{props.answerElements}</div>
-            })
-        })*/
-        setAnswers(prevAnswers => {
-            return prevAnswers.map(item => {
-                if (item.id === id) {
-                   return { ...item, isSelected: true}
-                }
-                return {...item, isSelected: false}
-            })
-        })
-
-    }
-
-
 
     return (
         <div className="question-block">
             <p>{props.ques}</p>
-            {answerEls}
+            <div className='answer-list'>{props.answerElements}</div>
             <span className="correctAnswer">{props.correctAnswer}</span>
         </div>
     )
